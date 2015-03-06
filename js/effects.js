7c0dfb8f3dd044ba64a2db48ecd8a0ea4c38b1d9
Tea Ho <tea@donorschoose.org>
2015-03-06 15:29:29 -0500

    Make responsive

--- js/effects.js
+++ js/effects.js
@@ -1,15 +1,22 @@
 $(function(){
+	// Here's How it Works Button Styles and Instructions
 	$('.ia-instructions-trigger').on('click', function(e){
 		e.preventDefault();
 		if(!($(this).hasClass('is-active'))){
+			// show or hide instructions
 			$('.ia-instructions-trigger').removeClass('is-active');
 			$('.ia-instructions').removeClass('is-visible');
 			$(this).addClass('is-active');
 			$(this).siblings('.ia-instructions').addClass('is-visible');
-		} else {
+
+			// switch instructional screenshot
+			var thisHowTo = $(this).attr('id');
+			var screenshotURL = "images/" + thisHowTo + ".png";
+			$('.ia-instruction-image').attr('src', screenshotURL);
 		}
 	});
 
+	// Show FAQ
 	$('.ia-faq-trigger').on('click', function(e){
 		$('.ia-faq').show();
 	})
