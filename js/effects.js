275149710b78597a3e422765335983dc676eeca4
Tea Ho <tea@donorschoose.org>
2015-03-06 10:57:33 -0500

    Add show and hide for buttons and fix number placement for firefox

---
+++ js/effects.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.ia-instructions-trigger').on('click', function(e){
+		e.preventDefault();
+		if(!($(this).hasClass('is-active'))){
+			$('.ia-instructions-trigger').removeClass('is-active');
+			$('.ia-instructions').removeClass('is-visible');
+			$(this).addClass('is-active');
+			$(this).siblings('.ia-instructions').addClass('is-visible');
+		} else {
+		}
+	});
+})
